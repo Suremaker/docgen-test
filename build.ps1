1b45153571482b066eee768b36e01ea640fda274
Wojtek <wojciech.kotlarski@gmail.com>
2017-05-24 00:49:34 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
 & .\nuget.exe install docfx.console -ExcludeVersion
+& .\nuget.exe install LightBDD.NUnit3 -ExcludeVersion
