7ed0070058e2c02fda52f52a84feb63375732f17
Wojtek <wojciech.kotlarski@gmail.com>
2017-05-24 12:27:19 +0100

    Delete build.ps1

--- build.ps1
+++
@@ -1,6 +0,0 @@
-wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
-& .\nuget.exe install docfx.console -ExcludeVersion
-& git clone https://github.com/LightBDD/LightBDD.git src
-& .\docfx.console\tools\docfx.exe .\docfx.json
-Remove-Item -recurse src
-Remove-Item -recurse docfx.console
